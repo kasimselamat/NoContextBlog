d8d1929b73086cd6b4f8dd148b8a277f94178fd4
panzul <95326601+kasimselamat@users.noreply.github.com>
2022-09-03 23:13:10 +0800

    Add files via upload

---
+++ latest.js
@@ -0,0 +1,22 @@
+﻿const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+const button = document.getElementById('on');
+const info = document.getElementById('info');
+const pic = document.getElementById('on');
+button.addEventListener('click',() => {
+    if (info.style.top == '100%'){
+        info.style.top = '0%';
+        info.style.opacity = '0';
+    }
+    else{
+        info.style.top = '100%' ;
+        info.style.opacity = '1';
+        // console.log('kambing')
+    }
+});
+
+hamburger.addEventListener('click',() => {
+    navUL.classList.toggle('show');
+});
+// var y = 'ikanayam';
+// document.getElementById('demo').innerHTML = y;
